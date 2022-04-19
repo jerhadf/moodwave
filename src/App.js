78b3645f3a4825e7cb229d9d4f4a0a9cee171c6d
Jaime <james.t.graft.23@dartmouth.edu>
2022-04-19 00:04:01 -0400

    fixed major issues with npm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test app with cool shit.
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TwitterLogin from "react-twitter-oauth";
+import TwitterLogin from "react-twitter-auth";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
